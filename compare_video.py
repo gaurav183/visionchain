9fadecce0c7aff113a3a380fb86692cb13932bb5
Gaurav Lahiry <glahiry@andrew.cmu.edu>
2017-07-21 13:43:35 -0400

    youtube video chain ability

--- compare_video.py
+++ compare_video.py
@@ -19,17 +19,28 @@ import hashlib
 # image = cv2.imdecode(np.fromstring(bmpData, dtype = np.uint8), 1)
 encoded_string = ""
 
-for i in xrange(30):
-	ffmpegCmd = "ffmpeg -i dash_2s.h264 -c:v libx264 -filter:v 'select=gte(n\,%d)' -frames:v 1 -f h264 frames/frame%d.h264"%(i,i)
-	output = sp.check_output([ffmpegCmd], shell=True)
+for i in xrange(10):
+	for j in xrange(30):
+		ffmpegCmd = "ffmpeg -i dash_cam.mp4 -c:v libx264 -filter:v 'select=gte(n\,%d)' -frames:v 1 -f h264 frames/frame%d.h264"%(j,j)
+		output = sp.check_output([ffmpegCmd], shell=True)
 
-	framePath = "frames/frame%d.h264"%i
-	with open(framePath, "rb") as image_file:
-		encoded_string += base64.b64encode(image_file.read())
+		framePath = "frames/frame%d.h264"%j
+		with open(framePath, "rb") as image_file:
+			encoded_string += base64.b64encode(image_file.read())
+
+	md5Hash = (hashlib.md5(encoded_string).hexdigest())
+	print "Hash MD5 = ", md5Hash
+	shaHash = (hashlib.sha512(encoded_string).hexdigest())
+	print "Hash SHA = ", shaHash
+
+	# send POST req to blockchain with shaHash, id = "dash_%d"%i
+
+	encoded_string = ""
 
 
-print "Hash MD5 = ", (hashlib.md5(encoded_string).hexdigest())
-print "Hash SHA = ", (hashlib.sha512(encoded_string).hexdigest())
 
-sys.stdout = open('dash2s30.txt', 'w')
+
+sys.stdout = open('dash30.txt', 'w')
 print encoded_string
+
+# 579d88bf0a07f3671427e2bf6d42fbe99420b8fb7ebf32ce4f24153b41d5b6c1b3cce86da0f53c1b0c1485df753bb0e21261adb6bc73dc7ef6428667ee19f61b
